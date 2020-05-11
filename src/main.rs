70abe543182575220019c096e6201e50c495a31f
mbarbie1 <michael.barbier@gmail.com>
2020-05-11 23:30:30 +0300

    Try out modules, structs, etc

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,16 @@
 mod space;
-/*
-mod space {
 
-    extern crate piston_window;
-    use piston_window::*;
-
-    pub fn init_game() {
-        let mut window: PistonWindow =
-            WindowSettings::new("Hello Piston!", [640, 480])
-                .exit_on_esc(true).build().unwrap();
-        while let Some(event) = window.next() {
-            window.draw_2d(&event, |context, graphics, _device| {
-                clear([1.0; 4], graphics);
-                rectangle([1.0, 0.0, 0.0, 1.0], // red
-                          [0.0, 0.0, 100.0, 100.0],
-                          context.transform,
-                          graphics);
-            });
-        }
-    }
-}
-*/
-//use crate::space;
+use space::Game;
 
 fn main() {
-    println!("Hello, Jupiter!");
-    space::init_game();
+    println!("This is Jupiter, the asteroids-like game.");
+
+    let game = Game {
+        width: 640,
+        height: 480,
+    };
+
+    game.init_game();
+
+    println!("Exit Jupiter");
 }
--- src/space.rs
+++ src/space.rs
@@ -1,10 +1,89 @@
+extern crate image as im;
 extern crate piston_window;
+extern crate find_folder;
 use piston_window::*;
+//use self::im::{ImageBuffer, GenericImage};
 
-pub fn init_game() {
+/*
+enum Ships {
+    Explorer(String),
+    Lander(String),
+}
+
+fn get_sprite(file_name: String, folder_path: String) {
+    let assets = find_folder::Search::ParentsThenKids(3, 3)
+        .for_folder(folder_path).unwrap();
+    let sprite = assets.join(file_name);
+    let sprite: G2dTexture = Texture::from_path(
+        &mut window.create_texture_context(),
+        &rust_logo,
+        Flip::None,
+        &TextureSettings::new()
+    ).unwrap();
+}
+
+
+impl Ships {
+    fn get_sprite(&self) {
+}
+
+let ship = Ships::Explorer(String::from("explorer.png"));
+ship.get_sprite();
+
+struct Ship {
+    x: f64,
+    y: f64,
+    vx: f64,
+    vy: f64,
+    ax: f64,
+    ay: f64,
+    angle: f64,
+    sprite: Image,
+}
+
+impl ship {
+
+    fn get_default_ship(x: f64, y: f64) {
+        let ship = Ship {
+            x,
+            y,
+            vx: 0.0,
+            vy: 0.0,
+            ax: 0.0,
+            ay: 0.0,
+            angle: 0.0,
+            sprite: get_sprite(, ),
+        }
+    }
+
+    fn update_position(&mut self) {
+        self.x = self.v;
+    }
+}
+*/
+
+struct Planet {
+    x: f64,
+    y: f64,
+    sprite: Image,
+}
+
+pub struct Game {
+    pub width: u32,
+    pub height: u32,
+    //pub ship: Ship,
+    //pub window: PistonWindow,
+    //pub planet: Planet,
+}
+
+impl Game {
+    pub fn init_game(self) {
         let mut window: PistonWindow =
-            WindowSettings::new("Hello Piston!", [640, 480])
+            WindowSettings::new("Jupiter", [640, 480])
                 .exit_on_esc(true).build().unwrap();
+
+        //let mut canvas = im::ImageBuffer::new(self.width, self.height);
+
         while let Some(event) = window.next() {
             window.draw_2d(&event, |context, graphics, _device| {
                 clear([1.0; 4], graphics);
@@ -15,3 +94,6 @@ pub fn init_game() {
             });
         }
     }
+}
+
+
